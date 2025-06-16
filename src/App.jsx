49d8deee3751213ef2ab21a9aae1def7c46da37e
edupoly <info.edupoly@gmail.com>
2025-06-16 07:22:35 +0530

    conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import StudentsTable from './student/StudentsTable';
 function App() {
   return (
-    <>
+    <div className='border border-danger border-5 m-5 p-5'>
       <h1>This is APP</h1>
       <StudentsTable></StudentsTable>
-    </>
+    </div>
   )
 }
 
