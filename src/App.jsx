f90adb1cb9d06659d281d0cdf34595155fdeeb54
edupoly <info.edupoly@gmail.com>
2025-06-17 07:20:40 +0530

    with style

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-
-
 function App() {
   return (
-    <>
+    <div className="border border-5 p-2 m-2 border-danger">
       <h1>This is APP</h1>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
