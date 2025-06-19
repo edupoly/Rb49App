34f8754e4ebd8673be7aa62de3fbf6a83427c209
edupoly <info.edupoly@gmail.com>
2025-06-19 07:22:52 +0530

    refactored-todolist-advanced

--- Todo.jsx
+++ Todo.jsx
@@ -3,9 +3,26 @@ import React from "react";
 function Todo(props) {
   return (
     <div>
-      <li className="border border-5 p-2 m-2 border-primary">
-        {props.t}
-        <button className="m-2 btn btn-danger">Delete</button>
+      <li className="border border-1 p-1 m-1 border-primary">
+        <span style={props.t.status ? { textDecoration: "line-through" } : {}}>
+          {props.t.title}
+        </span>
+        <button
+          className={props.t.status ? "btn btn-info" : "btn btn-success"}
+          onClick={() => {
+            props.toggleTodo(props.t.id);
+          }}
+        >
+          {props.t.status ? "Undo" : "Done"}
+        </button>
+        <button
+          className="m-1 btn btn-danger"
+          onClick={() => {
+            props.deleteTodo(props.t.id);
+          }}
+        >
+          Delete
+        </button>
       </li>
     </div>
   );
